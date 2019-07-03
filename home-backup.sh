e0465c1cd062e80f0896f0e9706ee9b79634d10d
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2019-07-02 19:32:36 -0600

    2 July 2019: some new utility scripts, plus small changes to home-backup.sh

--- home-backup.sh
+++ home-backup.sh
@@ -4,7 +4,7 @@
 # script no longer assumes it's running as root as of 11 July 2014.
 
 if [ -d /home/patrick/Photos/.NeverDelete ]; then      # the external photos drive is plugged in
-	echo "Pruning backup dir ..."; sleep 2; rsync -avhHSP --delete --delete-delay --log-file=/home/patrick/Desktop/home_backup.log --exclude ".thumbnails" --exclude ".gvfs" --exclude ".cache" -e ssh /home/patrick patrick@file-server:/media/Backups/Backups
+	echo "Pruning backup dir ..."; sleep 2; rsync -avhHSP --delete --ignore-errors --delete-delay --exclude ".thumbnails" --exclude ".gvfs" --exclude ".cache" --exclude ".temp" --exclude ".config/google-chrome/Default/Application Cache/" --exclude ".local/share/Trash" --exclude ".dropbox-dist/dropbox-lnx*" --exclude ".dropbox/" --exclude ".dropbox.cache" --exclude ".config/google-chrome" --exclude ".PyCharmCE2*" --exclude ".local/share/gvfs-metadata" --exclude ".config/libreoffice" --log-file=/home/patrick/Desktop/home_backup.log -e ssh /home/patrick patrick@file-server:/media/Backup/Backups
 else       # external Photos drive is missing -- update existing, but don't delete anything.
-	echo "Not pruning backup dir ..."; sleep 2; rsync -avhHSP --delete-delay --log-file=/home/patrick/Desktop/home_backup.log --exclude ".thumbnails" --exclude ".gvfs" --exclude ".cache" -e ssh /home/patrick patrick@file-server:/media/Backups/Backups
+	echo "Not pruning backup dir ..."; sleep 2; rsync -avhHSP --log-file=/home/patrick/Desktop/home_backup.log --exclude ".thumbnails" --exclude ".gvfs" --exclude ".cache" --exclude ".temp" --exclude ".config/google-chrome/Default/Application Cache/" --exclude ".local/share/Trash" --exclude ".dropbox-dist/dropbox-lnx*" --exclude ".dropbox/" --exclude ".dropbox.cache" --exclude ".config/google-chrome" --exclude ".PyCharmCE2*" --exclude ".local/share/gvfs-metadata" --exclude ".config/libreoffice" -e ssh /home/patrick patrick@file-server:/media/Backup/Backups
 fi
---
+++ sort-HDRs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir HDR non-HDR
+mv *jpg non-HDR/
+mv *HDR.JPG HDR/
+rm *cr2
