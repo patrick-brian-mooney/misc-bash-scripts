624a8ea3f4346763711a83a7268e1d17681e0fe3
Patrick Mooney <github.pmooney78@neverbox.com>
2020-10-15 14:17:09 -0600

    15 Oct 2020: adding a quick script to clean the Wine 'Open With...' list

---
+++ clean-wine-open-with-list.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rm -f ~/.local/share/mime/packages/x-wine*
+rm -f ~/.local/share/applications/wine-extension*
+rm -f ~/.local/share/icons/hicolor/*/*/application-x-wine-extension*
+rm -f ~/.local/share/mime/application/x-wine-extension*
+
+# based on https://wiki.winehq.org/FAQ#How_do_I_clean_the_Open_With_List.3F
--- update-all.sh
+++ update-all.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # Update all the software on every Linux machine on the network.
-sudo aptitude update; sudo aptitude upgrade
-ssh -t file-server 'sudo aptitude update; sudo aptitude upgrade'
-ssh -t liniscious 'sudo aptitude update; sudo aptitude upgrade'
+ssh -t linawesome 'sudo aptitude update; sudo aptitude upgrade; sudo apt-get autoremove; sudo apt-get -y clean'
+ssh -t file-server 'sudo aptitude update; sudo aptitude upgrade; sudo apt-get autoremove; sudo apt-get -y clean'
+ssh -t liniscious 'sudo aptitude update; sudo aptitude upgrade; sudo apt-get autoremove; sudo apt-get -y clean'
+ssh -t mahalo@web-server 'sudo aptitude update; sudo aptitude upgrade; sudo apt-get autoremove; sudo apt-get -y clean'
+
