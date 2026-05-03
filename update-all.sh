f145cfbbc471199a74e33cb18f4510647603095b
Patrick Mooney <github.pmooney78@neverbox.com>
2026-05-02 18:28:19 -0700

    Stop using obsolete login creds for update-all.sh

--- update-all.sh
+++ update-all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Update all the software on every Linux machine on the network.
-ssh -t linawesome 'sudo aptitude update; sudo aptitude upgrade; sudo apt-get autoremove; sudo apt-get -y clean'
-ssh -t file-server 'sudo aptitude update; sudo aptitude upgrade; sudo apt-get autoremove; sudo apt-get -y clean'
 ssh -t liniscious 'sudo aptitude update; sudo aptitude upgrade; sudo apt-get autoremove; sudo apt-get -y clean'
+ssh -t file-server 'sudo aptitude update; sudo aptitude upgrade; sudo apt-get autoremove; sudo apt-get -y clean'
+ssh -t media-server 'sudo aptitude update; sudo aptitude upgrade; sudo apt-get autoremove; sudo apt-get -y clean'
 ssh -t mahalo@web-server 'sudo aptitude update; sudo aptitude upgrade; sudo apt-get autoremove; sudo apt-get -y clean'
 
